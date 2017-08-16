db57efb858a150f37765d77269fa8634cefade7b
Chris Chung <christopher0chung@gmail.com>
2017-08-16 15:09:43 -0500

    Darkness tuning.

---
+++ Deeper (5.5.1f1 Migrated)/Assets/Scripts/Mechanical/Mech_SuitAirSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mech_SuitAirSystem : Deeper_Component
+{
+    private void Awake()
+    {
+        Initialize(1000);
+    }
+
+    public override void NormUpdate()
+    {
+
+    }
+}
