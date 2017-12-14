b95a76f665b02a89633900b8582ccc32757aa234
Chris Chung <christopher0chung@gmail.com>
2017-12-14 13:26:57 -0500

    Objective system in progress.

---
+++ Deeper (5.5.1f1 Migrated)/Assets/Scripts/Arch/Deeper_Objective.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObjectiveStates { Inactive, Active, Complete }
+
+public class Deeper_Objective
+{
+    public ObjectiveStates currentState;
+    public List<Deeper_Objective> activationCondition;
+}
+
+public class Deeper_Objective_Level1_GetToPlace : Deeper_Objective
+{
+    public Deeper_Objective_Level1_GetToPlace (List<Deeper_Objective> activationCond)
+    {
+        currentState = ObjectiveStates.Inactive;
+        activationCondition = activationCond;
+    }
+}
---
+++ Deeper (5.5.1f1 Migrated)/Assets/Scripts/Arch/Deeper_ObjectiveManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deeper_ObjectiveManager {
+
+    static private Deeper_ObjectiveManager _instance;
+    static public Deeper_ObjectiveManager instance
+    {
+        get
+        {
+            if (_instance == null)
+                return _instance = new Deeper_ObjectiveManager();
+            else
+                return _instance;
+        }
+    }
+
+    private List<Deeper_Objective> registeredObjectives;
+
+    public void Register(Deeper_Objective o)
+    {
+        Debug.Assert(!registeredObjectives.Contains(o), "Registered Objective already exists.");
+        registeredObjectives.Add(o);
+    }
+
+    //public void UpdateObjective<T>()
+}
